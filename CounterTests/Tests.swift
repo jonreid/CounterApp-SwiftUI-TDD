7e24c346a8c09ee1381ad7ff88209e09e73dbac7
Jon Reid <jon@qualitycoding.org>
2025-03-08 18:32:38 -0800

    . t Remove template tests, leave 1 failing test in place

---
+++ CounterTests/Tests.swift
@@ -0,0 +1,9 @@
+@testable import Counter
+import XCTest
+
+@MainActor
+final class Tests: XCTestCase, Sendable {
+    func test_zero() throws {
+        XCTFail("Tests not yet implemented in Tests")
+    }
+}
